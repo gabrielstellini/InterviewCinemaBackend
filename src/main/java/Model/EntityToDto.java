88731f032c9cb1fd1771950595bbcc7897153e42
Gabriel Stellini <gabrielstellini@gmail.com>
2018-10-06 12:54:45 +0200

    Add dto generic

---
+++ src/main/java/Model/EntityToDto.java
@@ -0,0 +1,15 @@
+package Model;
+
+import org.springframework.beans.BeanUtils;
+
+public abstract class EntityToDto<D, E>{
+    public D toDto(E entity, D dto) {
+        if(entity != null & dto != null) {
+            BeanUtils.copyProperties(entity, dto);
+            return dto;
+        }
+        else {
+            return null;
+        }
+    }
+}
